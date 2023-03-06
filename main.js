2021af2d5e4d2ff5362c1dd8edba4e1cc45a2a09
Robert Allred <rallred84@gmail.com>
2023-03-06 01:58:19 -0600

    first refactor commit... rewriting js from scratch to try and utilize new tecniques

--- main.js
+++ main.js
@@ -1,84 +1,94 @@
-let gameArea = document.getElementById("tic-tac-toe-board");
+const gameArea = document.getElementById('tic-tac-toe-board');
+
+const playingBoard = {
+  spot1: '1',
+  spot2: '2',
+  spot3: '3',
+  spot4: '4',
+  spot5: '5',
+  spot6: '6',
+  spot7: '7',
+  spot8: '8',
+  spot9: '9',
+  winningConditions: {
+    //Line 15 comes back as false
+    topRow: this.spot1 === this.spot2 && this.spot1 === this.spot3,
+    middleRow: this.spot4 === this.spot5 && this.spot4 === this.spot6,
+    bottomRow: this.spot7 === this.spot8 && this.spot7 === this.spot9,
+    leftColumn: this.spot1 === this.spot4 && this.spot1 === this.spot7,
+    middleColumn: this.spot2 === this.spot5 && this.spot2 === this.spot8,
+    rightColumn: this.spot3 === this.spot6 && this.spot3 === this.spot9,
+    diagonal1: this.spot1 === this.spot5 && this.spot1 === this.spot9,
+    diagonal2: this.spot7 === this.spot5 && this.spot7 === this.spot3,
+  },
+  checkForWinner: function () {
+    ///Line 26 comes back as true
+    console.log(this.spot1 === this.spot2 && this.spot1 === this.spot3);
+    for (condition in this.winningConditions) {
+      if (this.winningConditions[condition] === true) {
+        alert('Winner');
+      }
+    }
+  },
+};
 
 //Setting variables for different game phases
 let preGame = true;
 let blueTurn = false;
 let redTurn = false;
+let gameNumber = 1;
 
-function checkForWinner() {
-  let winConditions = [
-    //top row
-    spot1.innerText !== "" &&
-      spot1.innerText === spot2.innerText &&
-      spot1.innerText === spot3.innerText,
-    //middle row
-    spot4.innerText !== "" &&
-      spot4.innerText === spot5.innerText &&
-      spot4.innerText === spot6.innerText,
-    //bottom row
-    spot7.innerText !== "" &&
-      spot7.innerText === spot8.innerText &&
-      spot7.innerText === spot9.innerText,
-    //left column
-    spot1.innerText !== "" &&
-      spot1.innerText === spot4.innerText &&
-      spot1.innerText === spot7.innerText,
-    //middle column
-    spot2.innerText !== "" &&
-      spot2.innerText === spot5.innerText &&
-      spot2.innerText === spot8.innerText,
-    //right column
-    spot3.innerText !== "" &&
-      spot3.innerText === spot6.innerText &&
-      spot3.innerText === spot9.innerText,
-    //diagonal top left to bottom right
-    spot1.innerText !== "" &&
-      spot1.innerText === spot5.innerText &&
-      spot1.innerText === spot9.innerText,
-    //diagonal bottom left to top right
-    spot7.innerText !== "" &&
-      spot7.innerText === spot5.innerText &&
-      spot7.innerText === spot3.innerText,
-  ];
+function isRedTurn() {
+  blueTurn = false;
+  redTurn = true;
+  gameArea.classList.remove('blue');
+  gameArea.classList.add('red');
+}
 
-  for (condition of winConditions) {
-    if (condition === true) {
-      if (playCounter % 2 === 1) {
-        winAnnouncement.textContent = "Team allRed X won the game!!";
-        winAnnouncement.classList.add("red-shadow");
-      } else {
-        winAnnouncement.textContent = "Team allBlue O won the game!!";
-        winAnnouncement.classList.add("blue-shadow");
-      }
-      restartButton();
-      return true;
-    }
+function isBlueTurn() {
+  redTurn = false;
+  blueTurn = true;
+  gameArea.classList.remove('red');
+  gameArea.classList.add('blue');
+}
+
+function startGame() {
+  if (gameNumber % 2 === 1) {
+    isRedTurn();
+  } else {
+    isBlueTurn();
+  }
+  preGame = false;
+}
+
+function markBoard(clickedBox) {
+  let mark = clickedBox.id;
+  if (redTurn) {
+    clickedBox.classList.add('red-x');
+    document.getElementById(clickedBox.id).textContent = 'X';
+    playingBoard[mark] = 'X';
+    console.log(playingBoard);
+    playingBoard.checkForWinner();
+    isBlueTurn();
+  } else {
+    clickedBox.classList.add('blue-o');
+    document.getElementById(clickedBox.id).textContent = 'O';
+    playingBoard[mark] = 'O';
+    console.log(playingBoard);
+    playingBoard.checkForWinner();
+    isRedTurn();
   }
 }
 
-let clickSpots = [
-  "spot-1",
-  "spot-2",
-  "spot-3",
-  "spot-4",
-  "spot-5",
-  "spot-6",
-  "spot-7",
-  "spot-8",
-  "spot-9",
-];
-
-gameArea.addEventListener("click", function (selectSquare) {
+gameArea.addEventListener('click', (event) => {
+  const clickedBox = event.target;
   if (preGame === true) {
-    //Event Listener Commands during pre game
-    for (spot in clickSpots) {
-      if (selectSquare.target.id === clickSpots[spot]) {
-        console.log(`You have selected ${clickSpots[spot]}`);
-      }
+    if (clickedBox.id === 'spot5') {
+      clickedBox.classList.remove('starting-spot');
+      clickedBox.textContent = '';
+      startGame();
     }
-  } else if (redTurn === true) {
-    //Event Listener Commands during red turn
-  } else if (blueTurn === true) {
-    //Event Listener Commands during blue turn
+  } else {
+    markBoard(clickedBox);
   }
 });
